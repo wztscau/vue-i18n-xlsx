4bc71bd1f0fdf5160b6c1d71034fc41469c2a43a
wuzitao <wuztiao@kuaizi.co>
2019-07-22 18:52:54 +0800

    Add 遍历branchs

--- bin/i18n-xlsx.js
+++ bin/i18n-xlsx.js
@@ -3,6 +3,7 @@
 const path = require('path')
 const fs = require('fs')
 const Program = require('commander')
+const { exec, execSync } = require('child_process')
 
 Program
   .version('1.0.0')
@@ -25,4 +26,27 @@ if (!Program.path) {
 }
 
 let rootPath = Program.path || pwd
-require(path.join(__dirname, '../index.js'))(rootPath)
+let rootCwd = {cwd: rootPath}
+
+let branchsStr = execSync('git branch -l', rootCwd).toString()
+let branchs = branchsStr.split(/\n/).map(s => s.replace('*', '').trim()).filter(s => s)
+console.log(`branchs`, branchs)
+let currentBranch = branchsStr.match(/\*\s\b(.*)\b/m)[1]
+execSync('git add . & git stash', rootCwd)
+branchs.forEach(br => {
+  if (br !== currentBranch) {
+    let coSucc = false
+    while (!coSucc) {
+      try {
+        execSync(`git checkout ${br}`, rootCwd)
+        coSucc = true
+      } catch (e) {
+        // Sometimes would throw error 'Permission denied' in windows
+        execSync('git reset --hard & git clean -df', rootCwd)
+      }
+    }
+    // Collect all i18ns from different branchs into one excel
+    require(path.join(__dirname, '../index.js'))(rootPath)
+  }
+})
+execSync(`git checkout ${currentBranch} & git stash apply`, rootCwd)
